a62e916085ff9f0e9daa2a950d0c3f97b540ec4d
Aman Monga <amanmonga000@gmail.com>
2025-09-22 14:03:31 +0530

    Pglisting Page done

--- frontend/src/pages/pglist/models/pgdata.js
+++ frontend/src/pages/pglist/models/pgdata.js
@@ -68,5 +68,69 @@ export const PgData = [
         facilities : ["Laundry" , "Food", "WiFi", "Gym"],
         city : "Gurugram"
     },
+    {
+        id : 5,
+        name: "MG Club and Resorts",
+        description: "A luxurious resort located in the heart of Chandigarh.",
+        images : [
+           "pg data/pg5/IMG-20250806-WA0046.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0052.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0053.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0054.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0055.jpg"
+        ],
+        price : 14000,
+        discount : 500,
+        location : "Sector 15, Chandigarh",
+        locationLink : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27870.533525461677!2d75.73012479999998!3d29.170073600000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391233b67c4878d9%3A0x67d85cf672dde2e7!2sMG%20Club%20and%20Resorts!5e0!3m2!1sen!2sin!4v1755797518352!5m2!1sen!2sin",
+        occupancy : "single",
+        lookingFor : "students",
+        facilities : ["Laundry" , "Food", "WiFi", "Gym"],
+        city : "Gurugram"
+    },
+    {
+        id : 6,
+        name: "MG Club and Resorts",
+        description: "A luxurious resort located in the heart of Chandigarh.",
+        images : [
+           "pg data/pg5/IMG-20250806-WA0046.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0052.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0053.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0054.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0055.jpg"
+        ],
+        price : 14000,
+        discount : 500,
+        location : "Sector 15, Chandigarh",
+        locationLink : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27870.533525461677!2d75.73012479999998!3d29.170073600000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391233b67c4878d9%3A0x67d85cf672dde2e7!2sMG%20Club%20and%20Resorts!5e0!3m2!1sen!2sin!4v1755797518352!5m2!1sen!2sin",
+        occupancy : "single",
+        lookingFor : "students",
+        facilities : ["Laundry" , "Food", "WiFi", "Gym"],
+        city : "Gurugram"
+    },
+    {
+        id : 7,
+        name: "MG Club and Resorts",
+        description: "A luxurious resort located in the heart of Chandigarh.",
+        images : [
+           "pg data/pg5/IMG-20250806-WA0046.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0052.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0053.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0054.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0055.jpg"
+        ],
+        price : 14000,
+        discount : 500,
+        location : "Sector 15, Chandigarh",
+        locationLink : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27870.533525461677!2d75.73012479999998!3d29.170073600000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391233b67c4878d9%3A0x67d85cf672dde2e7!2sMG%20Club%20and%20Resorts!5e0!3m2!1sen!2sin!4v1755797518352!5m2!1sen!2sin",
+        occupancy : "single",
+        lookingFor : "students",
+        facilities : ["Laundry" , "Food", "WiFi", "Gym"],
+        city : "Gurugram"
+    },
+    {
+        id : 8,
+        name: "MG Club and Resorts",
+        description: "A luxurious resort located in the heart of Chandigarh.",
+        images : [
+           "pg data/pg5/IMG-20250806-WA0046.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0052.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0053.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0054.jpg","frontend/public/pg data/pg5/IMG-20250806-WA0055.jpg"
+        ],
+        price : 14000,
+        discount : 500,
+        location : "Sector 15, Chandigarh",
+        locationLink : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27870.533525461677!2d75.73012479999998!3d29.170073600000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391233b67c4878d9%3A0x67d85cf672dde2e7!2sMG%20Club%20and%20Resorts!5e0!3m2!1sen!2sin!4v1755797518352!5m2!1sen!2sin",
+        occupancy : "single",
+        lookingFor : "students",
+        facilities : ["Laundry" , "Food", "WiFi", "Gym"],
+        city : "Gurugram"
+    },
 
 ]
--- frontend/src/pages/pglist/view/extra.jsx
+++ frontend/src/pages/pglist/view/extra.jsx
@@ -1,3 +1,6 @@
+import { Filter } from "lucide-react"
+<>
+
  <div className=" flex gap-6 justify-between items-center border border-blue-300 shadow-2xs p-4 rounded-xl ">
                         <div className=" p-3 flex  gap-4 text-lg font-semibold rounded-xl">
                             <select name="city" id="" onChange={(event) => handlefilters({ title: "city", values: event.target.value.toString() })}>
@@ -61,3 +64,62 @@
                             </span>
                         </div>
                     </div>
+
+
+
+
+                     <div className=" w-max flex gap-6 justify-between items-center border border-gray-400 shadow-2xs p-4 rounded-xl ">
+                        <div className=" p-3 flex  gap-4 text-lg font-semibold rounded-xl">
+                            <select name="city" className=" focus:outline-0" id="" onChange={(event) => handlefilters({ title: "city", values: event.target.value.toString() })}>
+                                <option value="Select City">Select location</option>
+                                {
+                                    locationList.map((location) => {
+                                        return (
+                                            <option value={location}>{location}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                            <select name="price" className="focus: outline-0" id="" onChange={(event) => handlefilters({ title: "price", values: event.target.value })}>
+                                <option value="Select Budget">Select Budget</option>
+                                {
+                                    priceRange.map((price) => {
+                                        return (
+                                            <option value={price}>{price}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                            <input type="text" className="border p-1 w-3xs rounded-lg text-md placeholder:text-sm placeholder:font-medium placeholder:pl-2" placeholder="search for pg in your location" />
+                        </div>
+                        <div className="relative flex gap-4 ">
+                            <span className="text-md p-2 font-semibold border border-gray-400 rounded-3xl">Students Friendly</span>
+                            {/* <span className=" flex w-max gap-2 font-medium text-lg items-center" onClick={handleSort}>Sort By <FaChevronDown /></span> */}
+                            <span className=" flex w-max gap-2 font-medium text-lg items-center">
+                                {
+                                    <select name="sort" id="">
+                                        <option value="sort by">Sort by</option>
+                                        {
+                                            sortBy.map((ele) => {
+                                                return (
+                                                    <option value={ele}>{ele}</option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                 
+                                }
+                            </span>
+                        </div>
+                    </div> 
+
+</>
+
+
+
+
+// upper Filter
+
+
+
+
